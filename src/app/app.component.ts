2fb3c58d1b79b4017ce2b6906089e053a3d06b6d
PrasunKumarBiswas <prasun.kumar@yahoo.com>
2022-08-26 07:17:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dream-app';
+  title = 'dockerize an angular application';
 }
